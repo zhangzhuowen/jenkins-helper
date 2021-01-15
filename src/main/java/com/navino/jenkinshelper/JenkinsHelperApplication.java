e1904e6738f3d8cb7f27d15c455fb00b3de54596
zhangzhuowen <zhaozhuowen@navinfo.com>
2021-01-16 00:16:04 +0800

    项目初始搭建

---
+++ src/main/java/com/navino/jenkinshelper/JenkinsHelperApplication.java
@@ -0,0 +1,13 @@
+package com.navino.jenkinshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navino/jenkinshelper/JenkinsHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navino.jenkinshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
